3145c735b52e1a35d206d3b1cf96a3c86753390f
Zach Daniel <zachary.s.daniel@gmail.com>
2020-10-06 01:36:35 -0400

    feat: tests + fixes

---
+++ test/support/resources/post.ex
@@ -0,0 +1,29 @@
+defmodule AshCsv.Test.Post do
+  @moduledoc false
+  use Ash.Resource,
+    data_layer: AshCsv.DataLayer
+
+  csv do
+    create? true
+    columns [:id, :title, :score, :public]
+    file "test/data_files/posts.csv"
+  end
+
+  actions do
+    read(:read)
+    update(:update)
+    create(:create)
+    destroy(:destroy)
+  end
+
+  attributes do
+    attribute(:id, :uuid, primary_key?: true, default: &Ecto.UUID.generate/0)
+    attribute(:title, :string)
+    attribute(:score, :integer)
+    attribute(:public, :boolean)
+  end
+
+  relationships do
+    has_many(:comments, AshCsv.Test.Comment, destination_field: :post_id)
+  end
+end
