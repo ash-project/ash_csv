c63c6c6327ae4452c4297e532ed18e50267659a2
Zach Daniel <zach@zachdaniel.dev>
2023-07-13 08:48:34 -0400

    chore: update test config

--- config/config.exs
+++ config/config.exs
@@ -2,6 +2,7 @@ import Config
 
 config :ash, :use_all_identities_in_manage_relationship?, false
 config :ash, :validate_api_resource_inclusion?, false
+config :ash, :validate_api_config_inclusion?, false
 
 if Mix.env() == :dev do
   config :git_ops,
