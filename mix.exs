ad475a21fa544afe37faabe306ac25947300bda9
Zach Daniel <zach@zachdaniel.dev>
2023-09-26 23:30:54 -0400

    docs: update spark & ex_doc for improved docs

--- mix.exs
+++ mix.exs
@@ -123,7 +123,7 @@ defmodule AshCsv.MixProject do
     [
       {:ash, ash_version("~> 2.6")},
       {:csv, "~> 2.4"},
-      {:ex_doc, "~> 0.22", only: [:dev, :test], runtime: false},
+      {:ex_doc, github: "elixir-lang/ex_doc", only: [:dev, :test], runtime: false},
       {:ex_check, "~> 0.12.0", only: [:dev, :test]},
       {:credo, ">= 0.0.0", only: [:dev, :test], runtime: false},
       {:dialyxir, ">= 0.0.0", only: [:dev, :test], runtime: false},
