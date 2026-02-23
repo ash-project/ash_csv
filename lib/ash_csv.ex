770a915509f6e0a37766c69ba54e10757d6043fd
Leon Qadirie <39130739+leonqadirie@users.noreply.github.com>
2026-02-23 14:44:07 +0100

    chore: SPDX contributor links (#14)

--- lib/ash_csv.ex
+++ lib/ash_csv.ex
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2020 ash_csv contributors <https://github.com/ash-project/ash_csv/graphs.contributors>
+# SPDX-FileCopyrightText: 2020 ash_csv contributors <https://github.com/ash-project/ash_csv/graphs/contributors>
 #
 # SPDX-License-Identifier: MIT
 
